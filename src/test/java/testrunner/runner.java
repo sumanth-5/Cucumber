c54f52d57ff8bad5225c85372f88d1ae9ee04aaf
sumanth-5 <gudipati12281a0212@gmail.com>
2022-10-24 10:42:12 +0530

    cucumber commit

---
+++ src/test/java/testrunner/runner.java
@@ -0,0 +1,11 @@
+package testrunner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)				
+@CucumberOptions(features="Features",glue={"StepDefinition"})
+
+public class runner {
+
+}
